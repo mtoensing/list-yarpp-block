1619c0a408f594d122ba4baeb954ec99ac6cd59b
Marc Toensing <marc.toensing@gmail.com>
2025-11-21 19:00:32 +0100

    build fix

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'ac2f7307e96a5167b3ed');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e718f9c65d8c03bf40d0');
