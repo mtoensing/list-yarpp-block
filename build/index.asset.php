ad40beae3b7ff3ea9e31a9bf30b22b67c31d1076
Marc Tönsing <marc.toensing@gmail.com>
2026-03-27 16:54:12 +0200

    Update WordPress package dependencies

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '5fd5e90c5971256cc05b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '8420d218fe7a413b5218');
